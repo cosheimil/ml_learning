77a00a919a188ca33b58785bea7c868c94ac667d
Cosheimil <a.varvus@ya.ru>
2023-07-29 07:31:27 +0300

    Enveironment by Nix

---
+++ shell.nix
@@ -0,0 +1,58 @@
+with import <nixpkgs> { };
+
+let
+  pythonPackages = python3Packages;
+in pkgs.mkShell rec {
+  name = "Machine Learning Venv";
+  venvDir = "/home/artem/projects/ml_learning/ml-venv";
+  buildInputs = [
+    # A Python interpreter including the 'venv' module is required to bootstrap
+    # the environment.
+    pythonPackages.python
+
+    # This executes some shell code to initialize a venv in $venvDir before
+    # dropping into the shell
+    pythonPackages.venvShellHook
+
+    # Those are dependencies that we would like to use from nixpkgs, which will
+    # add them to PYTHONPATH and thus make them accessible from within the venv.
+    pythonPackages.numpy
+    pythonPackages.requests
+
+    # Working with data
+    pythonPackages.pandas
+    pythonPackages.numpy
+    pythonPackages.matplotlib
+    pythonPackages.seaborn
+
+    # Machine Learning
+    pythonPackages.scikit-learn
+    pythonPackages.xgboost
+    pythonPackages.lightgbm
+    pythonPackages.catboost
+    pythonPackages.torch
+    pythonPackages.torchvision
+    pythonPackages.torchaudio
+
+    # Web Frameworks
+    streamlit
+    pythonPackages.jupyter
+    pythonPackages.ipykernel
+    
+    # In this particular example, in order to compile any binary extensions they may
+    # require, the Python modules listed in the hypothetical requirements.txt need
+    # the following packages to be installed locally:
+    taglib
+    openssl
+    git
+    libxml2
+    libxslt
+    libzip
+    zlib
+  ];
+
+  # Run this command, only after creating the virtual environment
+  postVenvCreation = ''
+    unset SOURCE_DATE_EPOCH
+  '';
+}
